24d4dceae0d103e5fae4073ad43951ff560f93be
Abduvohid Ergashev <Ab.ergashev@anorbank.org>
2024-03-22 11:59:24 +0500

    custom gateway app

---
+++ src/test/java/uz/gc/gateway/CustomGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.gc.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
